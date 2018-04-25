59fafa0576479a5c328fb200c5db4b4f3e0b287b
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-04-24 20:10:30 -0700

    Added modules to proxy server

--- server.js
+++ server.js
@@ -10,13 +10,23 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/funding-rounds/:company', (req, res) => {
-	console.log('hello');
   fetch(`http://localhost:3001${req.originalUrl}`)
     .then(fetchRes => fetchRes.json())
     .then(data => res.send(data));
 
 });
 
+app.use('/overview/:orgId', (req, res) => {
+  fetch(`http://localhost:3002${req.originalUrl}`)
+    .then(fetchRes => fetchRes.json())
+    .then(data => res.send(data));
+});
+
+app.use('/people/:org', (req, res) => {
+  fetch(`http://localhost:3004${req.originalUrl}`)
+    .then(response => response.json())
+    .then(data => res.send(data));
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
